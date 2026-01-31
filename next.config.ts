397bc2d3e7f434445c6eaf3be64d39405b312b98
Ravikumar-2016 <ravikumargunti837@gmail.com>
2026-01-31 21:41:13 +0530

    removed unused files

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Ignore ESLint errors during the build
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Ignore TypeScript errors during the build
     ignoreBuildErrors: true,
